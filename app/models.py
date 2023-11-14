08254b2d959a9f86b8ecbf76e38900436971dec3
Andrey-Yarkov <yarkov.andrey.v@gmail.com>
2023-11-14 11:24:23 +0300

    Added rating system

--- app/models.py
+++ app/models.py
@@ -14,22 +14,21 @@ class QuestionManager(models.Manager):
     def newest(self):
         return self.order_by('-creation_time')
 
-    def get_answers_count(self):
-        answers = Answer.objects.filter(question=self)
-        return answers.count()
+    #def get_answers_count(self):
+    #    answers = Answer.objects.filter(question=self)
+    #    return answers.count()
 
     def sort_by_rating(self):
-        return self.all()
-
-    #def get_questions_by_tag(self, tag_item):
-
+        return self.order_by('-rating')
 
 class Question(models.Model):
     title = models.CharField(max_length=256)
     content = models.TextField()
     author = models.ForeignKey('Profile', max_length=256, on_delete=models.PROTECT)
-    tags = models.ManyToManyField('Tag', related_name='questions')
+    tags = models.ManyToManyField('Tag', related_name='questions', blank=True)
     creation_time = models.DateTimeField(default=datetime.now, blank=True)
+    rating = models.IntegerField(default=0, blank=True)
+    answers_count = models.IntegerField(default=0, blank=True)
 
     objects = QuestionManager()
 
@@ -40,6 +39,8 @@ class Question(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.PROTECT)
     avatar = models.ImageField()
+    rates = models.ForeignKey('Rate', null=True, blank=True, max_length=256, on_delete=models.PROTECT)
+    user_rating = models.IntegerField(default=0, blank=True)
 
     def __str__(self):
         return f"{self.user.username}"
@@ -55,6 +56,19 @@ class Tag(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class RateManager(models.Manager):
+    def get_question_rate(self, question):
+        rates = self.filter(question=question)
+        positive_rates = rates.filter(is_positive=True)
+        return 2 * positive_rates.count() - rates.count()
+
+class Rate(models.Model):
+    is_positive = models.BooleanField(null=True, blank=True)
+    question = models.ForeignKey('Question', max_length=256, on_delete=models.PROTECT)
+
+    objects = RateManager()
 
 
 class AnswerManager(models.Manager):
