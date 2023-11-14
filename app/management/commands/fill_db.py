70c2cc53a52d10ab18ac1fb21e3f6984f4b7d747
Andrey-Yarkov <yarkov.andrey.v@gmail.com>
2023-11-14 21:48:29 +0300

    Fill database script updated

--- app/management/commands/fill_db.py
+++ app/management/commands/fill_db.py
@@ -10,13 +10,15 @@ class Command(BaseCommand):
         parser.add_argument('--ratio', type=int)
 
     def handle(self, *args, **options):
+
         ratio = options['ratio']
 
         password = 'pass_new_test_123_1'
         users = [None] * ratio
         profiles = [None] * ratio
         for i in range(ratio):
-            users[i] = User(username='user' + str(i + 1), password=password)
+            users[i] = User(username='user' + str(i + 1),
+                            password=password)
             users[i].save()
             profiles[i] = Profile(user=users[i])
             profiles[i].save()
@@ -26,24 +28,29 @@ class Command(BaseCommand):
             tags[i] = Tag(name='tag' + str(i + 1))
             tags[i].save()
 
-        question_titles = ['A', 'B', 'C', 'D', 'E', 'F', 'J']
         questions = [None] * (ratio * 10)
         for i in range(ratio * 10):
-            questions[i] = Question(title=question_titles[i % 7] * (i % ratio + 1), content='QQ',
+            questions[i] = Question(title='Question №' + str(i+1),
+                                    content='Question text ' * 20,
                                     author=profiles[i % ratio])
             questions[i].save()
             questions[i].tags.add(tags[i % (ratio - 1)], tags[i % (ratio - 1) + 1])
             questions[i].save()
 
-        answers_content = ['H', 'I', 'J', 'K', 'L', 'M', 'N']
         answers = [None] * (ratio * 100)
         for i in range(ratio * 100):
-            answers[i] = Answer(content=answers_content[i % 7] * (i % ratio + 1), author=profiles[i % ratio],
+            answers[i] = Answer(content='Answer text ' * 20,
+                                author=profiles[i % ratio],
                                 question=questions[i % (ratio * 10)])
             answers[i].save()
 
 
-        #ans_rate = AnswerRate(is_positive=True, answer=answer, user=user1)
-        #ans_rate.save()
-        #qu_rate = QuestionRate(is_positive=False, question=question, user=user2)
-        #qu_rate.save()
+        for i in range(ratio * 200):
+            q_rate = QuestionRate(is_positive=True,
+                                  question=questions[i % (ratio * 10)],
+                                  user=profiles[i % ratio])
+            q_rate.save()
+            a_rate = AnswerRate(is_positive=True,
+                                answer=answers[i % (ratio * 100)],
+                                user=profiles[i % ratio])
+            a_rate.save()
