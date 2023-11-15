834831f65391c906f7722c6b81afab6b6c06c9f5
Andrey-Yarkov <yarkov.andrey.v@gmail.com>
2023-11-15 11:18:13 +0300

    Updated fill database script

--- app/management/commands/fill_db.py
+++ app/management/commands/fill_db.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from app.models import Question, Profile, Answer, Tag, QuestionRate, AnswerRate
 from django.contrib.auth.models import User
+import random
 
 
 class Command(BaseCommand):
@@ -10,7 +11,6 @@ class Command(BaseCommand):
         parser.add_argument('--ratio', type=int)
 
     def handle(self, *args, **options):
-
         ratio = options['ratio']
 
         password = 'pass_new_test_123_1'
@@ -23,34 +23,43 @@ class Command(BaseCommand):
             profiles[i] = Profile(user=users[i])
             profiles[i].save()
 
+        print('Profiles created')
+
         tags = [None] * ratio
         for i in range(ratio):
             tags[i] = Tag(name='tag' + str(i + 1))
             tags[i].save()
 
+        print('Tags created')
+
         questions = [None] * (ratio * 10)
         for i in range(ratio * 10):
-            questions[i] = Question(title='Question №' + str(i+1),
-                                    content='Question text ' * 20,
+            questions[i] = Question(title='Question №' + str(i + 1),
+                                    content='Question text ' * 10,
                                     author=profiles[i % ratio])
             questions[i].save()
             questions[i].tags.add(tags[i % (ratio - 1)], tags[i % (ratio - 1) + 1])
             questions[i].save()
 
+        print('Questions created')
+
         answers = [None] * (ratio * 100)
         for i in range(ratio * 100):
-            answers[i] = Answer(content='Answer text ' * 20,
+            answers[i] = Answer(content=str(i + 1) + ' answer text ' * 10,
                                 author=profiles[i % ratio],
                                 question=questions[i % (ratio * 10)])
             answers[i].save()
 
+        print('Answers created')
 
-        for i in range(ratio * 200):
-            q_rate = QuestionRate(is_positive=True,
+        for i in range(ratio * 100):
+            q_rate = QuestionRate(is_positive=random.choice([True, False]),
                                   question=questions[i % (ratio * 10)],
                                   user=profiles[i % ratio])
             q_rate.save()
-            a_rate = AnswerRate(is_positive=True,
+            a_rate = AnswerRate(is_positive=random.choice([True, False]),
                                 answer=answers[i % (ratio * 100)],
                                 user=profiles[i % ratio])
             a_rate.save()
+
+        print('Rates created')
